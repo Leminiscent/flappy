22e15de75d06eba553dcb7f4cd18d9ea2f0bf8ed
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-05-08 12:32:48 -0400

    chore: Refactor ScoreState to use preloaded medal images and display based on score
    The ScoreState.lua file has been refactored to use preloaded medal images instead of loading them dynamically. This change improves performance by reducing the number of file reads during runtime. Additionally, the code has been modified to display the medal based on the player's score. This enhances the visual feedback for the player.

--- states/ScoreState.lua
+++ states/ScoreState.lua
@@ -6,26 +6,12 @@
 
 ScoreState = Class { __includes = BaseState }
 
-local GOLD_IMAGE = love.graphics.newImage('gold.png')
-local SILVER_IMAGE = love.graphics.newImage('silver.png')
-local BRONZE_IMAGE = love.graphics.newImage('bronze.png')
-
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
 ]]
 function ScoreState:enter(params)
     self.score = params.score
-    -- Determine which medal to display
-    if self.score >= 30 then
-        self.medal = GOLD_IMAGE
-    elseif self.score >= 20 then
-        self.medal = SILVER_IMAGE
-    elseif self.score >= 10 then
-        self.medal = BRONZE_IMAGE
-    else
-        self.medal = nil
-    end
 end
 
 function ScoreState:update(dt)
@@ -36,16 +22,12 @@ function ScoreState:update(dt)
 end
 
 function ScoreState:render()
-    -- Display the score and the message
+    -- simply render the score to the middle of the screen
     love.graphics.setFont(flappyFont)
     love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
-
-    -- Display the medal if there is one
-    if self.medal then
-        love.graphics.draw(self.medal, VIRTUAL_WIDTH / 2 - self.medal:getWidth() / 2, 130)
-    end
 
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
 end
