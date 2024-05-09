fdce6dda20bc25dbfa06abd9b32330fa1b36e659
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-05-09 11:34:15 -0400

    feat: Toggle scrolling when pausing the game
    The PlayState.lua file has been modified to toggle the scrolling behavior when the game is paused. This improvement allows for a smoother gameplay experience by pausing the scrolling of game elements when the game is paused, and resuming scrolling when the game is resumed.

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -28,6 +28,7 @@ end
 function PlayState:update(dt)
     if love.keyboard.wasPressed('p') then
         self.isPaused = not self.isPaused
+        scrolling = not scrolling
     end
 
     if not self.isPaused then
